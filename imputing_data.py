7b5ae4ac310f759ccac98b476d6c48ed880c9a64
Sedar Olmez <k1462369@kcl.ac.uk>
2019-05-15 15:15:30 +0100

    Performed imputation on 2013 data. Created a few more graphs using seabn

--- imputing_data.py
+++ imputing_data.py
@@ -4,7 +4,7 @@
 # Promotion4 f2_score:      0.8915634612104615
 # Promotion5 f2_score:      0.5287041047093568
 # CPI f2_score       :      0.35543474289083
-# Weekly_Sales f2_score:    0.5117239482428886
+# Weekly_Sales f2_score:  0.5619225329783258
 
 import datawig
 import sklearn
@@ -12,14 +12,15 @@ import pandas as pd
 from sklearn.metrics import r2_score as score
 from sklearn.metrics import classification_report as cr
 
-df = pd.read_csv('filled_dataframe.csv')
+df = pd.read_csv('impute_2013_final.csv')
 df = df.fillna(0)
 
 df_train, df_test = datawig.utils.random_split(df, split_ratios=[0.8, 0.2])
 
 imputer = datawig.SimpleImputer(
-    input_columns = ['CPI', 'Temperature', 'Fuel_Price', 'Promotion1', 'Promotion2', 'Promotion3', 'Promotion4', 'Promotion5', 'Promotion1_imputed',
-    'Promotion2_imputed', 'Promotion4_imputed', 'Promotion3_imputed', 'Promotion5_imputed', 'CPI_imputed'],
+    input_columns = ['Store', 'Fuel_Price', 'Dept', 'Temperature', 'Promotion1', 'Promotion2', 'Promotion3',
+          'Promotion4', 'Promotion5', 'Promotion1_imputed', 'Promotion2_imputed', 'Promotion3_imputed',
+          'Promotion4_imputed', 'Promotion5_imputed', 'Unemployment', 'Size (sq ft)', 'CPI_imputed'],
     output_column = 'Weekly_Sales',
     output_path = 'imputer_model'
 )
@@ -30,7 +31,7 @@ imputed = imputer.predict(df_test)
 
 new_dataframe = pd.DataFrame(data = imputed)
 
-new_dataframe.to_csv('Weekly_Sales_imputed.csv')
+new_dataframe.to_csv('2013_sales_complete.csv')
 
 f1 = score(imputed['Weekly_Sales'], imputed['Weekly_Sales_imputed'])
 print('Weekly_Sales f2_score: ', f1)
